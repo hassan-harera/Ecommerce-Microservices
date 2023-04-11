eeb4f357716ed188cef63d6553fcb0a3e878aea2
Hassan Harera <Hassan.Shaban.Harera@gmail.com>
2023-04-11 17:01:40 +0200

    Add database migration service

---
+++ landscape/database-migration/src/main/java/com/harera/ecommerce/database/DatabaseMigrationApplication.java
@@ -0,0 +1,11 @@
+package com.harera.ecommerce.database;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabaseMigrationApplication {
+
+    public static void main(String[] args) {
+        org.springframework.boot.SpringApplication.run(DatabaseMigrationApplication.class, args);
+    }
+}
