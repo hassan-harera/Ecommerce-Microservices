48085bc10f7c56b94945b9e08c3373bcc62fb4ab
Hassan Harera <Hassan.Shaban.Harera@gmail.com>
2023-04-11 19:18:34 +0200

    Add gateway service

---
+++ landscape/gateway/src/main/java/com/harera/ecommerce/gateway/GatewayApplication.java
@@ -0,0 +1,37 @@
+package com.harera.ecommerce.gateway;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.reactive.config.EnableWebFlux;
+import reactor.core.publisher.Mono;
+
+import static org.springframework.boot.SpringApplication.run;
+
+@EnableWebFlux
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        run(GatewayApplication.class, args);
+    }
+
+    @Bean
+    public KeyResolver keyResolver() {
+        return exchange -> Mono.just(exchange.getRequest().getRemoteAddress().getAddress()
+                        .getHostAddress());
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**",
+                        new CorsConfiguration().applyPermitDefaultValues());
+        return source;
+    }
+}
